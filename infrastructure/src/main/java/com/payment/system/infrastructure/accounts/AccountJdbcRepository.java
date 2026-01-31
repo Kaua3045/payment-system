393ad3a4d1bc91a79f583a917415edfd3459cef6
Kauã Pereira <zkauacodes@hotmail.com>
2026-01-31 18:43:53 -0300

    feat(account-repository): added the account of id method implementation

--- infrastructure/src/main/java/com/payment/system/infrastructure/accounts/AccountJdbcRepository.java
+++ infrastructure/src/main/java/com/payment/system/infrastructure/accounts/AccountJdbcRepository.java
@@ -2,7 +2,13 @@ package com.payment.system.infrastructure.accounts;
 
 import com.payment.system.application.repositories.AccountRepository;
 import com.payment.system.domain.accounts.Account;
+import com.payment.system.domain.accounts.AccountId;
+import com.payment.system.domain.accounts.AccountStatus;
+import com.payment.system.domain.utils.ULID;
+import com.payment.system.domain.valueobjects.Money;
 import com.payment.system.infrastructure.jdbc.DatabaseClient;
+import com.payment.system.infrastructure.jdbc.JdbcUtils;
+import com.payment.system.infrastructure.jdbc.RowMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -10,7 +16,9 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 @Component
 public class AccountJdbcRepository implements AccountRepository {
@@ -36,6 +44,12 @@ public class AccountJdbcRepository implements AccountRepository {
         return anAccount;
     }
 
+    @Override
+    public Optional<Account> accountOfId(final String anId) {
+        final var aSql = "SELECT * FROM accounts WHERE id = :id";
+        return this.databaseClient.queryOne(aSql, Map.of("id", anId), accountMapper());
+    }
+
     private void create(final Account anAccount) {
         final var aSql = """
                 INSERT INTO accounts (id, user_id, balance, status, created_at, updated_at, closed_at, version)
@@ -57,5 +71,18 @@ public class AccountJdbcRepository implements AccountRepository {
         aParams.put("version", anAccount.getVersion());
 
         return this.databaseClient.update(aSql, aParams);
+    }
+
+    private RowMap<Account> accountMapper() {
+        return rs -> Account.with(
+                new AccountId(ULID.fromString(rs.getString("id"))),
+                rs.getLong("version"),
+                rs.getString("user_id"),
+                new Money(rs.getBigDecimal("balance")),
+                AccountStatus.from(rs.getString("status")).orElse(null),
+                JdbcUtils.getInstant(rs, "created_at"),
+                JdbcUtils.getInstant(rs, "updated_at"),
+                JdbcUtils.getInstant(rs, "closed_at")
+        );
     }
 }
