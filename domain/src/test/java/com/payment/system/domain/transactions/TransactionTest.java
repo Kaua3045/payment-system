668156d91190fce30443be1812166ccaa449e745
Kauã Pereira <zkauacodes@hotmail.com>
2026-02-01 19:58:39 -0300

    test(transaction): added the transaction aggregate root tests

---
+++ domain/src/test/java/com/payment/system/domain/transactions/TransactionTest.java
@@ -0,0 +1,101 @@
+package com.payment.system.domain.transactions;
+
+import com.payment.system.domain.UnitTest;
+import com.payment.system.domain.accounts.AccountId;
+import com.payment.system.domain.utils.IdentifierUtils;
+import com.payment.system.domain.utils.InstantUtils;
+import com.payment.system.domain.validation.handler.NotificationHandler;
+import com.payment.system.domain.valueobjects.Money;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+class TransactionTest extends UnitTest {
+
+    @Test
+    void givenAValidParams_whenCallsNewTransaction_thenInstantiateANewTransaction() {
+        final var aFromAccountId = new AccountId(IdentifierUtils.generateNewMonotonicULID());
+        final var aToAccountId = new AccountId(IdentifierUtils.generateNewMonotonicULID());
+        final var aAmount = new Money(new BigDecimal("150.00"));
+        final var anIdempotencyKey = "unique-key-123";
+
+        final var aTransaction = Transaction.newTransaction(
+                aFromAccountId,
+                aToAccountId,
+                aAmount,
+                anIdempotencyKey
+        );
+
+        Assertions.assertNotNull(aTransaction);
+        Assertions.assertEquals(aFromAccountId, aTransaction.getFromAccountId());
+        Assertions.assertEquals(aToAccountId, aTransaction.getToAccountId());
+        Assertions.assertEquals(aAmount, aTransaction.getAmount());
+        Assertions.assertEquals(TransactionStatus.PENDING, aTransaction.getStatus());
+        Assertions.assertEquals(anIdempotencyKey, aTransaction.getIdempotencyKey());
+        Assertions.assertNotNull(aTransaction.getCreatedAt());
+        Assertions.assertNotNull(aTransaction.getUpdatedAt());
+        Assertions.assertDoesNotThrow(() -> aTransaction.validate(NotificationHandler.create()));
+    }
+
+    @Test
+    void givenAValidValues_whenCallsWithTransaction_thenReturnANewTransactionWithUpdatedValues() {
+        final var aId = new TransactionId(IdentifierUtils.generateNewMonotonicULID());
+        final var aFromAccountId = new AccountId(IdentifierUtils.generateNewMonotonicULID());
+        final var aToAccountId = new AccountId(IdentifierUtils.generateNewMonotonicULID());
+        final var aAmount = new Money(new BigDecimal("150.00"));
+        final var anIdempotencyKey = "unique-key-123";
+        final var aStatus = TransactionStatus.PENDING;
+        final var aNow = InstantUtils.now();
+
+        final var aTransaction = Transaction.with(
+                aId,
+                1L,
+                aFromAccountId,
+                aToAccountId,
+                aAmount,
+                aStatus,
+                anIdempotencyKey,
+                aNow,
+                aNow
+        );
+
+        Assertions.assertNotNull(aTransaction);
+        Assertions.assertEquals(aId, aTransaction.getId());
+        Assertions.assertEquals(1L, aTransaction.getVersion());
+        Assertions.assertEquals(aFromAccountId, aTransaction.getFromAccountId());
+        Assertions.assertEquals(aToAccountId, aTransaction.getToAccountId());
+        Assertions.assertEquals(aAmount, aTransaction.getAmount());
+        Assertions.assertEquals(aStatus, aTransaction.getStatus());
+        Assertions.assertEquals(anIdempotencyKey, aTransaction.getIdempotencyKey());
+        Assertions.assertEquals(aNow, aTransaction.getCreatedAt());
+        Assertions.assertEquals(aNow, aTransaction.getUpdatedAt());
+        Assertions.assertDoesNotThrow(() -> aTransaction.validate(NotificationHandler.create()));
+    }
+
+    @Test
+    void givenAnInvalidStatusName_whenCallsTransactionStatusFrom_thenReturnEmpty() {
+        final var invalidStatusName = "INVALID_STATUS";
+
+        final var status = TransactionStatus.from(invalidStatusName);
+
+        Assertions.assertTrue(status.isEmpty());
+    }
+
+    @Test
+    void testCallToStringInTransaction() {
+        final var aFromAccountId = new AccountId(IdentifierUtils.generateNewMonotonicULID());
+        final var aToAccountId = new AccountId(IdentifierUtils.generateNewMonotonicULID());
+        final var aAmount = new Money(new BigDecimal("150.00"));
+        final var anIdempotencyKey = "unique-key-123";
+
+        final var aTransaction = Transaction.newTransaction(
+                aFromAccountId,
+                aToAccountId,
+                aAmount,
+                anIdempotencyKey
+        );
+
+        Assertions.assertNotNull(aTransaction.toString());
+    }
+}
