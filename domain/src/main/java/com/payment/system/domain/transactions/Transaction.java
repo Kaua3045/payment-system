383ca6b3f00682f7bf210a1c3ee66c96ac2c93f0
Kauã Pereira <zkauacodes@hotmail.com>
2026-02-01 20:56:18 -0300

    feat(transaction): added the complete method

--- domain/src/main/java/com/payment/system/domain/transactions/Transaction.java
+++ domain/src/main/java/com/payment/system/domain/transactions/Transaction.java
@@ -100,6 +100,11 @@ public class Transaction extends AggregateRoot<TransactionId> {
         );
     }
 
+    public void complete() {
+        this.setStatus(TransactionStatus.COMPLETED);
+        this.setUpdatedAt(InstantUtils.now());
+    }
+
     public AccountId getFromAccountId() {
         return fromAccountId;
     }
