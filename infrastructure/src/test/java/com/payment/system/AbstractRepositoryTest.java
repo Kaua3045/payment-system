6fbea3b2ac4238f3a799c480d056484c58177ad8
Kauã Pereira <zkauacodes@hotmail.com>
2026-02-01 21:55:25 -0300

    test(abstract-repository-test): added the transaction repository class

--- infrastructure/src/test/java/com/payment/system/AbstractRepositoryTest.java
+++ infrastructure/src/test/java/com/payment/system/AbstractRepositoryTest.java
@@ -2,9 +2,11 @@ package com.payment.system;
 
 import com.payment.system.application.repositories.AccountRepository;
 import com.payment.system.application.repositories.PixKeyRepository;
+import com.payment.system.application.repositories.TransactionRepository;
 import com.payment.system.infrastructure.accounts.AccountJdbcRepository;
 import com.payment.system.infrastructure.jdbc.JdbcClientAdapter;
 import com.payment.system.infrastructure.pixkeys.PixKeyJdbcRepository;
+import com.payment.system.infrastructure.transactions.TransactionJdbcRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,7 @@ public class AbstractRepositoryTest {
 
     private static final String ACCOUNTS_TABLE = "accounts";
     private static final String PIX_KEYS_TABLE = "pix_keys";
+    private static final String TRANSACTIONS_TABLE = "transactions";
 
     @Autowired
     private JdbcClient jdbcClient;
@@ -30,11 +33,13 @@ public class AbstractRepositoryTest {
 
     private AccountRepository accountRepository;
     private PixKeyRepository pixKeyRepository;
+    private TransactionRepository transactionRepository;
 
     @BeforeEach
     void setUp() {
         this.accountRepository = new AccountJdbcRepository(new JdbcClientAdapter(jdbcClient, operations));
         this.pixKeyRepository = new PixKeyJdbcRepository(new JdbcClientAdapter(jdbcClient, operations));
+        this.transactionRepository = new TransactionJdbcRepository(new JdbcClientAdapter(jdbcClient, operations));
     }
 
     protected int countAccounts() {
@@ -45,11 +50,19 @@ public class AbstractRepositoryTest {
         return JdbcTestUtils.countRowsInTable(jdbcClient, PIX_KEYS_TABLE);
     }
 
+    protected int countTransactions() {
+        return JdbcTestUtils.countRowsInTable(jdbcClient, TRANSACTIONS_TABLE);
+    }
+
     protected AccountRepository accountRepository() {
         return accountRepository;
     }
 
     protected PixKeyRepository pixKeyRepository() {
         return pixKeyRepository;
+    }
+
+    protected TransactionRepository transactionRepository() {
+        return transactionRepository;
     }
 }
