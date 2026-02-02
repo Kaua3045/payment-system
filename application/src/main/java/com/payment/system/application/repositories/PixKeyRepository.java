1b9cb816c6d52a65e889c78c1c55be8596438099
Kauã Pereira <zkauacodes@hotmail.com>
2026-02-01 21:16:15 -0300

    feat(pix-key-repository): added the pix key of active by value method

--- application/src/main/java/com/payment/system/application/repositories/PixKeyRepository.java
+++ application/src/main/java/com/payment/system/application/repositories/PixKeyRepository.java
@@ -2,9 +2,13 @@ package com.payment.system.application.repositories;
 
 import com.payment.system.domain.pixkeys.PixKey;
 
+import java.util.Optional;
+
 public interface PixKeyRepository {
 
     PixKey save(PixKey pixKey);
 
     boolean existsByValue(String value);
+
+    Optional<PixKey> pixKeyOfActiveByValue(String value);
 }
