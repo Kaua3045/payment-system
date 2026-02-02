41005a52a57a967511f5cb15dc04525c222233a7
Kauã Pereira <zkauacodes@hotmail.com>
2026-02-01 21:53:58 -0300

    feat(transaction-migrations): added the transaction migrations

---
+++ infrastructure/src/main/resources/db/migration/V3__create-transactions-table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE transactions (
+    id VARCHAR(26) NOT NULL PRIMARY KEY,
+    from_account_id VARCHAR(26) NOT NULL,
+    to_account_id VARCHAR(26) NOT NULL,
+    piux_key_id VARCHAR(26) NOT NULL,
+    version BIGINT NOT NULL DEFAULT 0,
+    amount NUMERIC(10, 2) NOT NULL,
+    status VARCHAR(15) NOT NULL,
+    idempotency_key VARCHAR(255) NOT NULL UNIQUE,
+    failure_reason VARCHAR(255) NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL
+);
