8c9073551143aac1db7b7e8fd2db75c0fcaf3310
Kauã Pereira <zkauacodes@hotmail.com>
2026-02-02 21:40:50 -0300

    refactor(create-transaction-usecase): refactor to make correct status in transaction

--- application/src/main/java/com/payment/system/application/usecases/transactions/create/DefaultCreateTransactionUseCase.java
+++ application/src/main/java/com/payment/system/application/usecases/transactions/create/DefaultCreateTransactionUseCase.java
@@ -44,58 +44,70 @@ public class DefaultCreateTransactionUseCase extends CreateTransactionUseCase {
             throw new UseCaseInputCannotBeNullException(CreateTransactionUseCase.class);
         }
 
-        if (this.transactionRepository.existsByIdempotencyKey(input.idempotencyKey())) {
-            throw DomainException.with("Transaction with idempotencyKey %s already exists".formatted(input.idempotencyKey()));
+        try {
+            return this.transactionManager.execute(() -> {
+                if (this.transactionRepository.existsByIdempotencyKey(input.idempotencyKey())) {
+                    throw DomainException.with("Transaction with idempotencyKey %s already exists".formatted(input.idempotencyKey()));
+                }
+
+                if (input.amount().compareTo(BigDecimal.ZERO) <= 0) {
+                    throw DomainException.with("Amount must be greater than zero");
+                }
+
+                final var aFromAccount = this.accountRepository.accountOfId(input.fromAccountId())
+                        .orElseThrow(NotFoundException.with(Account.class, input.fromAccountId()));
+
+                if (!aFromAccount.getStatus().equals(AccountStatus.ACTIVE)) {
+                    throw DomainException.with("From account is not active");
+                }
+
+                final var aPixKeyType = PixKeyType.from(input.pixKeyType())
+                        .orElseThrow(() -> NotFoundException.with("PixKeyType %s not found".formatted(input.pixKeyType())));
+
+                final var aKey = new PixKeyValueFactory().create(aPixKeyType, input.pixKey());
+
+                final var aPixKey = this.pixKeyRepository.pixKeyOfActiveByValue(aKey.value())
+                        .orElseThrow(NotFoundException.with(PixKey.class, "value", input.pixKey()));
+
+                final var aToAccount = this.accountRepository.accountOfId(aPixKey.getAccountId().value().toString())
+                        .orElseThrow(NotFoundException.with(Account.class, aPixKey.getAccountId().value().toString()));
+
+                if (!aToAccount.getStatus().equals(AccountStatus.ACTIVE)) {
+                    throw DomainException.with("To account is not active");
+                }
+
+                final var aTransaction = Transaction.newTransaction(
+                        aFromAccount.getId(),
+                        aToAccount.getId(),
+                        aPixKey.getId(),
+                        new Money(input.amount()),
+                        TransactionType.TRANSFER,
+                        input.idempotencyKey()
+                );
+
+                this.transactionRepository.save(aTransaction);
+
+                aFromAccount.debit(input.amount());
+                aToAccount.credit(input.amount());
+
+                this.accountRepository.save(aFromAccount);
+                this.accountRepository.save(aToAccount);
+
+                aTransaction.complete();
+                this.transactionRepository.save(aTransaction);
+
+                return CreateTransactionOutput.from(aTransaction);
+            });
+        } catch (final Exception ex) {
+            this.transactionManager.execute(() -> {
+                this.transactionRepository.transactionOfIdempotencyKey(input.idempotencyKey())
+                        .ifPresent(tx -> {
+                            tx.fail(ex.getMessage());
+                            this.transactionRepository.save(tx);
+                        });
+                return null;
+            });
+           throw ex;
         }
-
-        return this.transactionManager.execute(() -> {
-            if (input.amount().compareTo(BigDecimal.ZERO) <= 0) {
-                throw DomainException.with("Amount must be greater than zero");
-            }
-
-            final var aFromAccount = this.accountRepository.accountOfId(input.fromAccountId())
-                    .orElseThrow(NotFoundException.with(Account.class, input.fromAccountId()));
-
-            if (!aFromAccount.getStatus().equals(AccountStatus.ACTIVE)) {
-                throw DomainException.with("From account is not active");
-            }
-
-            final var aPixKeyType = PixKeyType.from(input.pixKeyType())
-                    .orElseThrow(() -> NotFoundException.with("PixKeyType %s not found".formatted(input.pixKeyType())));
-
-            final var aKey = new PixKeyValueFactory().create(aPixKeyType, input.pixKey());
-
-            final var aPixKey = this.pixKeyRepository.pixKeyOfActiveByValue(aKey.value())
-                    .orElseThrow(NotFoundException.with(PixKey.class, "value", input.pixKey()));
-
-            final var aToAccount = this.accountRepository.accountOfId(aPixKey.getAccountId().value().toString())
-                    .orElseThrow(NotFoundException.with(Account.class, aPixKey.getAccountId().value().toString()));
-
-            if (!aToAccount.getStatus().equals(AccountStatus.ACTIVE)) {
-                throw DomainException.with("To account is not active");
-            }
-
-            final var aTransaction = Transaction.newTransaction(
-                    aFromAccount.getId(),
-                    aToAccount.getId(),
-                    aPixKey.getId(),
-                    new Money(input.amount()),
-                    TransactionType.TRANSFER,
-                    input.idempotencyKey()
-            );
-
-            this.transactionRepository.save(aTransaction);
-
-            aFromAccount.debit(input.amount());
-            aToAccount.credit(input.amount());
-
-            this.accountRepository.save(aFromAccount);
-            this.accountRepository.save(aToAccount);
-
-            aTransaction.complete();
-            this.transactionRepository.save(aTransaction);
-
-            return CreateTransactionOutput.from(aTransaction);
-        });
     }
 }
