6f52c2d1d934b0e51a8e65300ad91a1ff7a2d698
Kauã Pereira <zkauacodes@hotmail.com>
2026-02-07 10:38:57 -0300

    feat(transaction-manager): added the generated class to impl integration

--- infrastructure/src/main/java/com/payment/system/infrastructure/wrapper/SpringTransactionManager.java
+++ infrastructure/src/main/java/com/payment/system/infrastructure/wrapper/SpringTransactionManager.java
@@ -1,12 +1,14 @@
 package com.payment.system.infrastructure.wrapper;
 
 import com.payment.system.application.wrapper.TransactionManager;
+import com.payment.system.domain.utils.Generated;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.function.Supplier;
 
+@Generated
 @Component
 public class SpringTransactionManager implements TransactionManager {
 
