cd958100216edb891dc1cfa6a5c3eb09267e44e2
Kauã Pereira <zkauacodes@hotmail.com>
2026-02-09 17:18:25 -0300

    feat(pix-key-api): added the list pix keys route method implementation

--- infrastructure/src/main/java/com/payment/system/infrastructure/rest/controllers/PixKeyRestController.java
+++ infrastructure/src/main/java/com/payment/system/infrastructure/rest/controllers/PixKeyRestController.java
@@ -2,9 +2,14 @@ package com.payment.system.infrastructure.rest.controllers;
 
 import com.payment.system.application.usecases.pixkeys.create.CreatePixKeyCommand;
 import com.payment.system.application.usecases.pixkeys.create.CreatePixKeyUseCase;
+import com.payment.system.application.usecases.pixkeys.retrieve.list.ListPixKeysUseCase;
+import com.payment.system.domain.pagination.Pagination;
+import com.payment.system.domain.pagination.SearchQuery;
+import com.payment.system.domain.utils.Period;
 import com.payment.system.infrastructure.idempotency.IdempotencyKey;
 import com.payment.system.infrastructure.pixkeys.req.CreatePixKeyRequest;
 import com.payment.system.infrastructure.pixkeys.res.CreatePixKeyResponse;
+import com.payment.system.infrastructure.pixkeys.res.ListPixKeysResponse;
 import com.payment.system.infrastructure.rest.PixKeyAPI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +17,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
 import java.util.Objects;
 
 @RestController
@@ -20,11 +27,14 @@ public class PixKeyRestController implements PixKeyAPI {
     private static final Logger log = LoggerFactory.getLogger(PixKeyRestController.class);
 
     private final CreatePixKeyUseCase createPixKeyUseCase;
+    private final ListPixKeysUseCase listPixKeysUseCase;
 
     public PixKeyRestController(
-            final CreatePixKeyUseCase createPixKeyUseCase
+            final CreatePixKeyUseCase createPixKeyUseCase,
+            final ListPixKeysUseCase listPixKeysUseCase
     ) {
         this.createPixKeyUseCase = Objects.requireNonNull(createPixKeyUseCase);
+        this.listPixKeysUseCase = Objects.requireNonNull(listPixKeysUseCase);
     }
 
     @IdempotencyKey
@@ -43,5 +53,39 @@ public class PixKeyRestController implements PixKeyAPI {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(CreatePixKeyResponse.from(aOutput));
+    }
+
+    @Override
+    public Pagination<ListPixKeysResponse> listPixKeys(
+            final Map<String, String> filters,
+            final String search,
+            final int page,
+            final int perPage,
+            final String sort,
+            final String direction,
+            final String startDate,
+            final String endDate
+    ) {
+        final var aQuery = SearchQuery.newSearchQuery(
+                page,
+                perPage,
+                search,
+                sort,
+                direction,
+                new Period(Period.startValidate(
+                        startDate,
+                        30,
+                        ChronoUnit.DAYS
+                ), Period.endValidate(
+                        endDate,
+                        30,
+                        ChronoUnit.DAYS
+                )),
+                filters
+        );
+
+        final var aOutput = this.listPixKeysUseCase.execute(aQuery);
+
+        return aOutput.map(ListPixKeysResponse::from);
     }
 }
