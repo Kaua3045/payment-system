ecef3304f706f996e18e640782b765ee4b74739b
Kauã Pereira <zkauacodes@hotmail.com>
2026-02-17 19:53:04 -0300

    feat(list-pix-keys-output): added the pix key formatter presentation to value

--- application/src/main/java/com/payment/system/application/usecases/pixkeys/retrieve/list/ListPixKeysOutput.java
+++ application/src/main/java/com/payment/system/application/usecases/pixkeys/retrieve/list/ListPixKeysOutput.java
@@ -1,5 +1,6 @@
 package com.payment.system.application.usecases.pixkeys.retrieve.list;
 
+import com.payment.system.application.usecases.pixkeys.presentations.PixKeyFormatter;
 import com.payment.system.domain.pixkeys.PixKey;
 
 import java.time.Instant;
@@ -19,7 +20,7 @@ public record ListPixKeysOutput(
         return new ListPixKeysOutput(
                 aKey.getId().value().toString(),
                 aKey.getAccountId().value().toString(),
-                aKey.getKey().value(),
+                PixKeyFormatter.format(aKey.getKey().value(), aKey.getKey().type()),
                 aKey.getKey().type().name(),
                 aKey.getStatus().name(),
                 aKey.getCreatedAt(),
