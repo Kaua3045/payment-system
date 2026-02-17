62dfb0e9cb11d6f3a3a524abb69d7ea0a309a811
Kauã Pereira <zkauacodes@hotmail.com>
2026-02-17 20:10:46 -0300

    feat(pix-key-repository): refactor list implementation to use key value nedded key type

--- infrastructure/src/main/java/com/payment/system/infrastructure/pixkeys/PixKeyJdbcRepository.java
+++ infrastructure/src/main/java/com/payment/system/infrastructure/pixkeys/PixKeyJdbcRepository.java
@@ -80,14 +80,12 @@ public class PixKeyJdbcRepository implements PixKeyRepository {
         if (query.terms() != null && !query.terms().isBlank()) {
             sql.append("""
                         AND (
-                            key_value ILIKE :terms
-                            OR type ILIKE :terms
+                            type ILIKE :terms
                         )
                     """);
             countSql.append("""
                         AND (
-                            key_value ILIKE :terms
-                            OR type ILIKE :terms
+                            type ILIKE :terms
                         )
                     """);
             params.put("terms", "%" + query.terms() + "%");
@@ -237,6 +235,16 @@ public class PixKeyJdbcRepository implements PixKeyRepository {
                     sql.append(" AND type = :type ");
                     countSql.append(" AND type = :type ");
                     params.put("type", value);
+                }
+                case "value" -> {
+                    final var aPixKeyType = PixKeyType.from(filters.get("type"))
+                            .orElseThrow(() -> NotFoundException.with("PixKeyType required on use value to search"));
+
+                    final var normalized = new PixKeyValueFactory().create(aPixKeyType, value).value();
+
+                    sql.append(" AND key_value = :value ");
+                    countSql.append(" AND key_value = :value ");
+                    params.put("value", normalized);
                 }
             }
         });
