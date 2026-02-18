becc795b338fe992e9d157f34163877c1589c471
Kauã Pereira <zkauacodes@hotmail.com>
2026-02-18 18:48:43 -0300

    test(metrics): added the metrics bean and configure import in tests

--- infrastructure/src/test/java/com/payment/system/IntegrationTest.java
+++ infrastructure/src/test/java/com/payment/system/IntegrationTest.java
@@ -1,6 +1,7 @@
 package com.payment.system;
 
 import com.payment.system.infrastructure.configurations.WebServerConfig;
+import com.payment.system.infrastructure.wrapper.OpenTelemetryMetrics;
 import org.junit.jupiter.api.Tag;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -14,7 +15,8 @@ import java.lang.annotation.*;
 @SpringBootTest(classes = {
         WebServerConfig.class,
         IntegrationTestConfig.class,
-        ObservationTest.OpenTelemetryTestConfig.class
+        ObservationTest.OpenTelemetryTestConfig.class,
+        OpenTelemetryMetrics.class
 })
 @Tag("integrationTest")
 public @interface IntegrationTest {
