e841059c2dd23b2efe4b938055244c1a32c47662
Kauã Pereira <zkauacodes@hotmail.com>
2026-02-21 21:42:43 -0300

    feat(metrics): added the attributes

--- application/src/main/java/com/payment/system/application/wrapper/Metrics.java
+++ application/src/main/java/com/payment/system/application/wrapper/Metrics.java
@@ -1,10 +1,12 @@
 package com.payment.system.application.wrapper;
 
+import java.util.Map;
+
 public interface Metrics {
 
-    void incrementCounter(String name, long value);
+    void incrementCounter(String name, long value, Map<String, String> attributes);
 
-    void recordTime(String name, long milliseconds);
+    void recordTime(String name, long milliseconds, Map<String, String> attributes);
 
-    void gauge(String name, long value);
+    void gauge(String name, long value, Map<String, String> attributes);
 }
