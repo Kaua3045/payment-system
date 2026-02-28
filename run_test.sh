ac674fba3a60fd42148c977ebc75412f94300468
Kauã Pereira <zkauacodes@hotmail.com>
2026-02-28 16:44:42 -0300

    build(docker-tests): added the run tests scripts in root folder

--- run_test.sh
+++ run_test.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 SCRIPT_NAME=$1
+ENVIRONMENT=${2:-dev}
 
 if [ -z "$SCRIPT_NAME" ]; then
-  echo "Uso: $0 <nome_do_script.js>"
+  echo "Use: $0 <script_name.js> [env]"
   exit 1
 fi
 
-docker-compose run --rm k6 /tests/$SCRIPT_NAME
-docker-compose down -v
+docker-compose -f infra/tests/docker/docker-compose.yml run --rm \
+  -e ENV=$ENVIRONMENT \
+  k6 \
+  /tests/$SCRIPT_NAME
+
+docker-compose -f infra/tests/docker/docker-compose.yml down -v
